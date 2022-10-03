4a0a1239cc8028e443a5f4e2c71f4cb52a70a7a7
Natanael Machado <natanaelsm0102@gmail.com>
2022-10-02 22:42:36 -0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from re import template
+from tkinter import Button
 from flask import Flask, render_template
 app = Flask("__name__")
 
@@ -7,13 +8,15 @@ app = Flask("__name__")
 def home ():
     return render_template("home.html")
 
+
 @app.route("/quemsomos")
 def quemsomos():
     return render_template("quemsomos.html")
+    
 
 @app.route("/contato")
 def contato():
     return render_template("contato.html")
 
-if __name__=='__name__':
+if __name__=='__main__':
     app.run(debug=True)
